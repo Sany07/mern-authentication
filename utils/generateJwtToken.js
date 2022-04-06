43cef1218b31d6c7f636750b7425ccf98d5730a2
Ahasan Habib Sany <sany.baust@gmail.com>
2022-04-06 14:54:37 +0600

    frontend updated

--- utils/generateJwtToken.js
+++ utils/generateJwtToken.js
@@ -1,11 +1,10 @@
-const jwt = require('jsonwebtoken')
-
+const jwt = require("jsonwebtoken");
 
 // @desc Generate JWT
 const generateJwtToken = (id) => {
-    return jwt.sign({ id }, process.env.JWT_SECRET, {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
     expiresIn: process.env.JWT_Expires,
-    })
-}
+  });
+};
 
-module.exports = generateJwtToken
+module.exports = generateJwtToken;
